9a0155332926e888af227a81d75dfaa933e23bf3
Simo Zoltan <zoltan.simo@msg-systems.com>
2017-02-20 16:27:13 +0200

    Bad user list with delayed return in User management added.

--- userapp-web/src/main/java/solomonj/msg/userapp/web/UserManagedBean.java
+++ userapp-web/src/main/java/solomonj/msg/userapp/web/UserManagedBean.java
@@ -32,6 +32,7 @@ public class UserManagedBean implements Serializable {
 	private boolean edit;
 	private List<String> selectedRoles = new ArrayList<>();
 	private List<User> allUsers = null;
+	private List<User> allBadUsers = null;
 	private String searchName = "";
 
 	private IUserService getUserBean() {
@@ -102,13 +103,27 @@ public class UserManagedBean implements Serializable {
 		try {
 			allUsers = getUserBean().searchUserByName(searchName);
 		} catch (ServiceException e) {
-			e.printStackTrace();
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR,
+					LoginManagedBean.getResourceBundleString(e.getMessage()), null));
 		}
 		if (allUsers == null) {
 			return new ArrayList<>();
 		}
 		return allUsers;
-
+	}
+	
+	public List<User> getAllBadUsers() {
+		try {
+			allBadUsers = getUserBean().getAllBadUsers();
+			return allBadUsers;
+		} catch (ServiceException e) {
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR,
+					LoginManagedBean.getResourceBundleString(e.getMessage()), null));
+		}
+		if (allBadUsers == null) {
+			return new ArrayList<>();
+		}
+		return allBadUsers;
 	}
 
 	public void insertUser(User user) {
@@ -184,6 +199,15 @@ public class UserManagedBean implements Serializable {
 			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR,
 					LoginManagedBean.getResourceBundleString(e.getMessage()), null));
 			return new User();
+		}
+	}
+
+	public void sendEmail(User u) {
+		try {
+			//sendemail 
+		} catch (Exception e) {
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR,
+					LoginManagedBean.getResourceBundleString(e.getMessage()), null));
 		}
 	}
 
