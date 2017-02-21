6c7f22458fcac118162a8598fe6f5e0e8e483034
szocsc <szocsc@MSGN04468.int.root.msg.ag>
2017-02-21 14:52:37 +0100

    email kuldese, gombbal mukodik

--- userapp-ejb/src/main/java/solomonj/msg/userapp/ejb/service/util/SendEmail.java
+++ userapp-ejb/src/main/java/solomonj/msg/userapp/ejb/service/util/SendEmail.java
@@ -1,11 +1,65 @@
 package solomonj.msg.userapp.ejb.service.util;
 
+import java.util.Date;
 import java.util.Properties;
 
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import org.jboss.logging.Logger;
+
+import solomonj.msg.appuser.common.exception.ServiceException;
+import solomonj.msg.userapp.ejb.repository.bean.BorrowingRepositoryBean;
+import solomonj.msg.userapp.ejb.repository.bean.UserRepositoryBean;
+import solomonj.msg.userapp.jpa.model.User;
+
 public class SendEmail {
-	 String to = "almapistike@yahoo.com";
-	 String from = "almapistike@yahoo.com";
-	 String host = "localhost";
-	 Properties properties = System.getProperties();
+	private Logger oLogger = Logger.getLogger(UserRepositoryBean.class);
+	
+	public static void sendEmail(String to, String from) throws ServiceException {
+
+		Properties props = new Properties();
+		props.setProperty("mail.transport.protocol", "smtp");
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.port", "587");
+		props.put("mail.smtp.starttls.enable", "true");
+
+		Session session = Session.getInstance(props, new Authenticator() {
+			@Override
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication("szocscsillamaria", "aranyalma");
+			}
+		});
+
+		// compose the message
+		try {
+			MimeMessage message = new MimeMessage(session);
+
+			message.setFrom(new InternetAddress(from));
+			message.setRecipient(Message.RecipientType.TO, new InternetAddress(to));
+			message.setSubject("Ping");
+			message.setSentDate(new Date());
+			message.setText("Hello, this is example of sending email " );
+
+			// Send message
+			Transport transport = session.getTransport("smtp");
+			transport.connect("smtp.gmail.com", "szocscsillamaria", "aranyalma");
+			transport.sendMessage(message, message.getAllRecipients());
+			transport.close();
+
+			// Transport.send(message);
+			System.out.println("message sent successfully....");
+
+		} catch (MessagingException mex) {
+			throw new ServiceException("error.email.failed");
+		}
+	}
 
 }
--- userapp-ejb/src/main/java/solomonj/msg/userapp/ejb/service/util/ShowTime.java
+++ userapp-ejb/src/main/java/solomonj/msg/userapp/ejb/service/util/ShowTime.java
@@ -1,5 +1,7 @@
 package solomonj.msg.userapp.ejb.service.util;
 
+import java.util.List;
+
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.ejb.Schedule;
@@ -7,24 +9,36 @@ import javax.ejb.Singleton;
 import javax.ejb.Startup;
 
 import solomonj.msg.userapp.ejb.repository.IBorrowingRepository;
+import solomonj.msg.userapp.ejb.repository.IUserRepository;
+import solomonj.msg.userapp.ejb.repository.bean.UserRepositoryBean;
+import solomonj.msg.userapp.ejb.repository.exception.RepositoryException;
+import solomonj.msg.userapp.jpa.model.User;
 
 @Singleton
 @Startup
 public class ShowTime {
-
 	@EJB
-	private IBorrowingRepository borrowingRepositoryBean;
+	private IUserRepository userRepositoryBean;
 
-	@Schedule(second = "*/1", minute = "*", hour = "*", persistent = false)
+	
+
+	@Schedule(second = "*/30", minute = "*", hour = "*", persistent = false)
 	public void checkBorrowing() {
-//		LIst<Borrowing> asd = borrowingRepositoryBean.getValami();
-//		id = asd.getUserId();
-//		userBean.getUserById(id).getEmail;
-//		IEmailApi.sendEmai()
+		try {
+			List<User> asd = userRepositoryBean.getAllBadUsers();
+//			System.out.println("check borrowing method");
+//			System.out.println("number of users " + asd.size());
+			//SendEmail.sendEmail("szocscsillamaria@gmail.com", "szocscsillamaria@gmail.com");
+		
+		} catch (RepositoryException e) {
+
+			e.printStackTrace();
+		}
+
 	}
 
 	@PostConstruct
 	public void startUp() {
-		//showTime();
+		 //checkBorrowing();
 	}
 }
