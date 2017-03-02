26c681ef67ae5111789c7cc56323895b2083dd7d
Jozsef Solomon <Jozsef.Solomon@msg-systems.com>
2017-03-02 14:17:59 +0200

    UserServiceTest

--- userapp-ejb/src/test/java/solomonj/msg/userapp/ejb/service/bean/UserServiceBeanTest.java
+++ userapp-ejb/src/test/java/solomonj/msg/userapp/ejb/service/bean/UserServiceBeanTest.java
@@ -10,9 +10,13 @@ import java.util.List;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import solomonj.msg.appuser.common.exception.ServiceException;
 import solomonj.msg.userapp.ejb.repository.IUserRepository;
+import solomonj.msg.userapp.ejb.repository.bean.BorrowingRepositoryBean;
+import solomonj.msg.userapp.ejb.repository.bean.UserRepositoryBean;
 import solomonj.msg.userapp.ejb.repository.exception.RepositoryException;
 import solomonj.msg.userapp.jpa.model.User;
 
@@ -21,30 +25,27 @@ public class UserServiceBeanTest {
 
 	/**
 	 *** My first simple mock test
+	 * 
 	 * @author Solomon Jozsef
 	 */
-	
+
 	@InjectMocks
-	IUserRepository userRepository = mock(IUserRepository.class);
-	
+	UserServiceBean userServiceBean;
+
+	@Mock
+	UserRepositoryBean userRepositoryBean;
+
 	@Test
-	public void searchUser() {
-		
+	public void searchUser() throws Exception {
+
 		User user = new User();
 		user.setUsername("Robi");
 		List<User> users = new ArrayList<>();
 		users.add(user);
-		
-		try {
-			when(userRepository.searchUserByName("Robi")).thenReturn(users);
-		} catch (RepositoryException e) {
-			e.printStackTrace();
-		}
-		try {
-			assertEquals("Robi",userRepository.searchUserByName("Robi").get(0).getUsername());
-		} catch (RepositoryException e) {
-			e.printStackTrace();
-		}
-		
+
+		when(userRepositoryBean.searchUserByName("Robi")).thenReturn(users);
+
+		assertEquals("Robi", userServiceBean.searchUserByName("Robi").get(0).getUsername());
+
 	}
 }
