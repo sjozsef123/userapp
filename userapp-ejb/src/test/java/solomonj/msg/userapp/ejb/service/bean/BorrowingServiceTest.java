c7875f8defed831c7e7050355fb1b0536919094e
Jozsef Solomon <Jozsef.Solomon@msg-systems.com>
2017-03-06 09:40:31 +0200

    borrowingServiceTest

--- userapp-ejb/src/test/java/solomonj/msg/userapp/ejb/service/bean/BorrowingServiceTest.java
+++ userapp-ejb/src/test/java/solomonj/msg/userapp/ejb/service/bean/BorrowingServiceTest.java
@@ -35,63 +35,90 @@ public class BorrowingServiceTest {
 
 	@InjectMocks
 	BorrowingServiceBean borrowingService;
-	
+
 	@Mock
 	IUserRepository userRepository;
-	
+
 	@Mock
 	IPubRepository pubRepository;
-	
+
 	@Mock
 	IBorrowingRepository borrowRepository;
-	
+
 	@Mock
 	PublicationBorrowing borrowing;
-	
+
 	@Mock
 	PublicationBorrowingPK pB;
 
-	
 	@Test(expected = ServiceException.class)
 	public void returnPubAfterDeadline() throws Exception {
-	
+
 		Publication book = new Book();
-		book.setCopiesLeft(1);		
+		book.setCopiesLeft(1);
 		User user = new User();
 		user.setLoyaltyIndex(1);
-	
-		
+
 		when(pubRepository.getPublicationById(anyInt())).thenReturn(book);
 		doNothing().when(pubRepository).update(book);
 		when(borrowing.getDeadline()).thenReturn(Date.valueOf(LocalDate.now().minusDays(1)));
 		when(borrowRepository.getBorrowById(any())).thenReturn(borrowing);
-		
+
 		when(userRepository.getUserById(anyInt())).thenReturn(user);
 		doNothing().when(userRepository).update(user);
 		doThrow(new RepositoryException("")).when(borrowRepository).deleteBorrowing(any());
 		PublicationBorrowingPK borrowingPK = new PublicationBorrowingPK();
-		
+
 		borrowingService.returnPublication(borrowingPK);
-		
+
 		assertEquals(2, book.getCopiesLeft());
 		assertEquals(0, user.getLoyaltyIndex());
-		
+
 	}
-	
+
 	@Test(expected = ServiceException.class)
 	public void borrowPubNoStock() throws Exception {
 		Publication book = new Book();
-		
-		
 		book.setCopiesLeft(0);
-		
-	    when(borrowing.getId()).thenReturn(pB);
-	   when(pB.getPublicationId()).thenReturn(5);
+		when(borrowing.getId()).thenReturn(pB);
+		when(pB.getPublicationId()).thenReturn(5);
 		when(pubRepository.getPublicationById(anyInt())).thenReturn(book);
 		borrowingService.borrowPublication(borrowing);
+
+	}
+
 	
+	@Test(expected = ServiceException.class)
+	public void borrowPubWithLowLoyalty() throws Exception {
+		User user = new User();
+		user.setLoyaltyIndex(0);
+		Publication book = new Book();
+		book.setCopiesLeft(1);
+		when(borrowing.getId()).thenReturn(pB);
+		when(pB.getPublicationId()).thenReturn(5);
+		when(pubRepository.getPublicationById(anyInt())).thenReturn(book);
+		when(pB.getUserId()).thenReturn(5);
+		when(userRepository.getUserById(anyInt())).thenReturn(user);
 		
+		
+		borrowingService.borrowPublication(borrowing);
 		
 	}
 	
+	@Test
+	public void borrowPub() throws Exception {
+		User user = new User();
+		user.setLoyaltyIndex(1);
+		Publication book = new Book();
+		book.setCopiesLeft(1);
+		when(borrowing.getId()).thenReturn(pB);
+		when(pB.getPublicationId()).thenReturn(5);
+		when(pubRepository.getPublicationById(anyInt())).thenReturn(book);
+		when(pB.getUserId()).thenReturn(5);
+		when(userRepository.getUserById(anyInt())).thenReturn(user);
+		doNothing().when(pubRepository).update(any());
+		doNothing().when(borrowRepository).insertBorrowing(borrowing);
+		borrowingService.borrowPublication(borrowing);
+		assertEquals(0, book.getCopiesLeft());
+	}
 }
